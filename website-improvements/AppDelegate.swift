b66e5b1aa088728bf988688fc382e8790046500a
Sebastian Livoni <29739749+sebastianlivoni@users.noreply.github.com>
2023-04-10 20:37:30 +0200

    Initial Commit

---
+++ website-improvements/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  website-improvements
+//
+//  Created by Sebastian Livoni on 10/04/2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
